97e9590525b4aedb9874913516e478113cd6e9bd
jensenjamison <jensenjamison@yahoo.com>
2019-09-06 16:50:55 -0500

    commit 2

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -3,10 +3,33 @@ import React, {Component} from "react";
 
 
 export default class Form extends Component {
+    constructor(){
+        super();
+        this.state = {
+            image: false,
+            product: false,
+            price: 0
+        }
+        this.handleInput = this.handleInput.bind(this)
+
+    }
+
+    handleInput(e) {
+        this.setState({[e.target.name] : e.target.value }) 
+    }
+
+
     render(){
         return(
             <div>
-                
+                <form>
+                    <input onChange = {(e) => this.setState({image: e.target.value})}></input>
+                    <input onChange = {(e) => this.setState({product: e.target.value})}></input>
+                    <input onChange = {(e) => this.setState({price: e.target.value})}></input>
+                </form>
+                    <button>Cancel</button>
+                    <button>Add to Inventory</button>
+
             </div>
         )
     }
